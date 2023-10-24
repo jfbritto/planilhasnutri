506e43c976047dda06f7b2e414f19da5490c0b33
João Filipi <jf.britto@hotmail.com>
2023-10-24 01:33:15 -0300

    meta img

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -5,8 +5,8 @@
 
     <!-- Meta Tags Open Graph para compartilhamento em redes sociais -->
     <meta property="og:title" content="PlanilhasNUTRI">
-    <meta property="og:description" content="Sistema de controle e administração da segurança dos alimentos">
-    <meta property="og:image" content="/img/logo.png">
+    <meta property="og:description" content="Controle e administração da segurança dos alimentos">
+    <meta property="og:image" content="/img/share.png">
     <meta property="og:url" content="https://planilhas.helpdiet.com.br/">
     <meta property="og:type" content="website">
 
